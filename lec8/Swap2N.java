1937f6ebf45e86152f02874f1d041de620355e0b
Ishant Yadav <ishant3010y@gmail.com>
2026-01-29 01:51:26 +0530

    Swapnum.

---
+++ lec8/Swap2N.java
@@ -0,0 +1,17 @@
+package lec8;
+
+public class Swap2N {
+    public static void main(String[] args) {
+        
+        int a = 45;
+        int b = 18;
+
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+
+        
+    }
+}
