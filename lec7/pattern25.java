9e54d6c04cce85192fd1ce6c5e464f79242da7b9
Ishant Yadav <ishant3010y@gmail.com>
2026-01-31 16:56:23 +0530

    var

---
+++ lec7/pattern25.java
@@ -0,0 +1,37 @@
+package lec7;
+
+public class pattern25 {
+    public static void main(String[] args) {
+        int n= 5;
+        int row =1;
+        int var =1;
+        int space =n-1;
+        int star =1;
+         
+        while(row <=n){
+            // Space
+            int i=1;
+            while (i <= space){
+                System.out.print(" \t");
+                i++;
+            }
+            // Star
+            int j=1;
+            
+            while(j <= star){
+                System.out.print(var+"\t");
+                j++;
+                var++;
+            }
+
+            row++;
+            star+=2;
+            space--;
+            
+            
+            System.out.println();
+        }
+
+    }
+    
+}
