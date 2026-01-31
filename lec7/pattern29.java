0c84cfaa8195c7207e2c2c353ac701b947da33ad
Ishant Yadav <ishant3010y@gmail.com>
2026-02-01 02:52:13 +0530

    var

---
+++ lec7/pattern29.java
@@ -0,0 +1,38 @@
+package lec7;
+
+public class pattern29 {
+    public static void main(String[] args){
+        int n=5;
+        int row=1;
+        int star=1;
+        int space=n-1;
+        
+        while(row<=n){
+            // space
+            int i =1;
+            while(i <=space){
+                System.out.print(" \t");
+                i++;
+            }
+
+            // Star
+            int j=1;
+            int val= row;
+            while(j<=star){
+                if(j ==1 || j == star){
+                 System.out.print(val+"\t");
+                }
+                else{
+                    System.out.print("0\t");
+                }
+                j++;             
+            }
+            row++;
+            space--;          
+            star+=2;
+            System.out.println();
+        }
+
+    }
+    
+}
