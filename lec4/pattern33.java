e66cebda3738c59746be517c8892a029ad85dc85
Ishant Yadav <ishant3010y@gmail.com>
2026-02-03 00:30:48 +0530

    33done

--- lec4/pattern33.java
+++ lec4/pattern33.java
@@ -1,43 +1,44 @@
 package lec4;
-
-public class pattern33 {
+public class pattern33{
     public static void main(String[] args){
-    int n =10;
-    int row =1;
-    int space = n-1;
-    int star =1;
-    int val =n;
+        int n =10;
+        int row =1;
+        int space =n;
+        int star = 1;
+        int var =n;
 
-    while(row <= n){
-        // Space
-        int i =1;
-        while(i <= space){
-            System.out.print(" \t");
-            i++;
+        while(row<=n){
+            // space
+            int i =1;
+            while(i<=space){
+                System.out.print(" \t");
+                i++;
+                
+            }
+            //Star
+            int j=1;
+            while(j<=star){
+                if( var == n){
+                    System.out.print("0\t");
+                }
+                else{
+                System.out.print(var+"\t");
+                }
+
+                if(j<row){
+                    var++;
+                }
+                else{
+                    var--;
+                }
+
+                j++;
+            }
+            
+            row++;
+            space--;
+            star+=2;
+            System.out.println();
         }
-
-        // Star
-        int j = 1;
-        while(j <= star){
-            if (val == 0){
-                System.out.print("0");
-            }
-            else{
-                System.out.print(val+" \t");
-            }
-            if(j <row){
-                val++;
-            }
-            else{
-                val--;
-            }
-        }
-        row++;
-        space--;
-        star+=2;
-        System.out.println();
-
     }
-}
-    
 }
