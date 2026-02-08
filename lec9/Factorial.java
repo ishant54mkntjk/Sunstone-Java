55d081933ce0c57919098de2ab5c1c5d62fc79c5
Ishant Yadav <ishant3010y@gmail.com>
2026-02-08 13:03:14 +0530

    up

--- lec9/Factorial.java
+++ lec9/Factorial.java
@@ -3,7 +3,7 @@ package lec9;
 public class Factorial {
     public static void main(String[] args) {
         
-        int fact = 1;
+        int fact = 5;
         int n=10;
 
         for(int i = 1; i <=n; i++){
---
+++ practices/fibonacci.java
@@ -0,0 +1,43 @@
+package practices;
+import java.util.*;
+public class fibonacci {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();   
+        int row =1;
+        int star =1;
+        int a =1;
+        int b =1;
+        int c =a+b;
+
+        while(row <=n){
+            int i=1;
+            
+            while(i<=star){
+                if(row ==1 ){
+                    System.out.print(0);
+                }
+                else if(row ==2){
+                System.out.print(1);
+                }
+
+                else if(row>2){
+                    System.out.print(c);
+                    a =b;
+                    b=c;
+                    
+                }
+
+                i++;
+            }
+           
+
+            row++;
+            star++;
+            c =a+b;
+            
+            System.out.println();
+            
+        }
+    }
+}    
