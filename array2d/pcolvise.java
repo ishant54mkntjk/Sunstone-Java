128e9f12ecc8b0b1d24c67b5adc724914394d6d4
Ishant Yadav <ishant3010y@gmail.com>
2026-04-09 23:34:33 +0530

    printcolwise

---
+++ array2d/pcolvise.java
@@ -0,0 +1,15 @@
+package array2d;
+
+public class pcolvise {
+    public static void main(String[] args){
+        // int[][] arr =new int[3][4];
+        int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(arr[j][i]+" ");
+            }
+             System.out.println();
+        }
+       
+    }
+}
